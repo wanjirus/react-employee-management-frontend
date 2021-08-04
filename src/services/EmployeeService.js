293b9e216f2a449f0cdd82e8e737cd12065422d1
USER <stanmburu254@gmail.com>
2021-08-04 10:48:23 +0300

    add employee component form

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -4,5 +4,8 @@ class EmployeeService{
     getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL,employee)
+    }
 }
 export default new EmployeeService()
